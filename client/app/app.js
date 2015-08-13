a5e2154ab72fc0f2614268aaeb10e3b092969d87
Charles Sipe <csipe84@gmail.com>
2015-08-12 18:38:16 -0700

    Add ngtouch module

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,7 @@ angular.module('topProgrammingBlogsApp', [
   'ngSanitize',
   'ui.router',
   'ui.bootstrap',
-  //'feeds',
+  'ngTouch',
   'firebase'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
